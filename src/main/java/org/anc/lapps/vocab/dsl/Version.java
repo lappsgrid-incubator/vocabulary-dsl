84033c1a0cb7cb04dfed7e44e6d8271c0bb2ebf8
Keith Suderman <suderman@anc.org>
2015-01-11 17:39:56 -0500

    Bumper version for next snapshot.

--- src/main/java/org/anc/lapps/vocab/dsl/Version.java
+++ src/main/java/org/anc/lapps/vocab/dsl/Version.java
@@ -6,7 +6,7 @@ package org.anc.lapps.vocab.dsl;
  */
 public final class Version
 {
-	private static final String version = "1.1.0";
+	private static final String version = "1.1.1";
 
 	public static String getVersion() { return version; }
 }
