8463d3b9d1d045e476e539a93a05b69d0db368dd
Keith Suderman <suderman@cs.vassar.edu>
2016-02-04 18:43:00 -0500

    Added support for required properties.

--- src/main/groovy/org/anc/lapps/vocab/dsl/PropertyDelegate.groovy
+++ src/main/groovy/org/anc/lapps/vocab/dsl/PropertyDelegate.groovy
@@ -6,6 +6,8 @@ package org.anc.lapps.vocab.dsl
 class PropertyDelegate {
     String type
     String description
+    boolean required = false;
+    boolean requiredSet = false;
 
     void setProperty(String name, value) {
         throw new MissingPropertyException("Unknown property ${name}")
@@ -17,5 +19,23 @@ class PropertyDelegate {
 
     void description(String description) {
         this.description = description
+    }
+
+    void required(boolean required) {
+        if (requiredSet) {
+            throw new VocabularyException("The required property has already been set.")
+        }
+        requiredSet = true
+        this.required = required
+        if (required) {
+            println "Found a required property."
+        }
+        else {
+            println "Found an optional property."
+        }
+    }
+
+    void optional(boolean optional) {
+        required(!optional)
     }
 }
