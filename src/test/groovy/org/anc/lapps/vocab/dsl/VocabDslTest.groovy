a9f3772cf42135751c4bc3bfadc8eae8242cdce0
Keith Suderman <suderman@cs.vassar.edu>
2019-03-13 18:00:55 -0400

    Added tests

--- src/test/groovy/org/anc/lapps/vocab/dsl/VocabDslTest.groovy
+++ src/test/groovy/org/anc/lapps/vocab/dsl/VocabDslTest.groovy
@@ -1,7 +1,47 @@
+/*
+ * Copyright (c) 2019 The American National Corpus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.anc.lapps.vocab.dsl
+
+import org.junit.Ignore
+import org.junit.Test
 
 /**
  *
  */
+@Ignore
 class VocabDslTest {
+
+    @Test
+    void generateTTL() {
+        String[] args = "-r ttl src/test/resources/lapps.vocab".split()
+        VocabDsl.main(args)
+    }
+
+    @Test
+    void generateExampleVocabulary() {
+        ['ttl', 'owl', 'rdf', 'jsonld'].each { String format ->
+            String[] args = "-r $format src/test/resources/example.vocabulary".split()
+            VocabDsl.main(args)
+        }
+    }
+
+    @Test
+    void generateExampleTTL() {
+        String[] args = "-r ttl src/test/resources/example.vocabulary".split()
+        VocabDsl.main(args)
+    }
 }
